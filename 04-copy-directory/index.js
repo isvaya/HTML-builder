bcd4320f3609bac7ded07c0354b1e1be3b973f6f
Isvaya <tasyana.pulatova@gmail.com>
2025-01-20 23:58:15 +0500

    fix: change 4 task

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -6,6 +6,11 @@ async function copyDir() {
   try {
     await fs.mkdir(newFolder, { recursive: true });
     const allFiles = await fs.readdir(dirPath, { withFileTypes: true });
+    const deleteFiles = await fs.readdir(newFolder, { withFileTypes: true });
+    for (const fileD of deleteFiles) {
+      const filePath = path.join(newFolder, fileD.name);
+      await fs.rm(filePath, { recursive: true, force: true });
+    }
     for (const file of allFiles) {
       const dirFilePath = path.join(dirPath, file.name);
       const newFilePath = path.join(newFolder, file.name);
